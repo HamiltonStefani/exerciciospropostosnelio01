4c862cf1dba9f9f000f7359d2b6d6254b9fc65f3
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2022-12-28 11:32:13 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package exerciciospropostosnelio01;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num01, num02, soma;
+	    System.out.println("Digite o primeiro número: ");
+		num01 = sc.nextInt();
+		System.out.println("Digite o segundo número: ");
+		num02 = sc.nextInt();
+		soma = num01 + num02;
+		System.out.println("O valor da soma é: " + soma);
+		
+	
+	}
+
+}
